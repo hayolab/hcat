de962b028b710b51c9908c12ba2d59215822e547
nabeen <nabeen@users.noreply.github.com>
2023-05-15 23:58:05 +0900

    feat: cat main func

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main (main) where
 
-import Lib
+import System.Environment (getArgs)
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "No files provided"
+    _ -> mapM_ printContent args
+
+printContent :: FilePath -> IO ()
+printContent path = do
+  content <- readFile path
+  putStr content
